c7328931d6a4e74297719dba87702f78dae228da
wsruser4 <adreyseminforws@yandex.ru>
2021-03-31 10:29:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewjava/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.recyclerviewjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.cpp, R.drawable.csh, R.drawable.python, R.drawable.js, R.drawable.java};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.programming_langueages);
+        s2 = getResources().getStringArray(R.array.descriptions);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
